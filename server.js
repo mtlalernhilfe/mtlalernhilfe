9c93fe2ef10a726057a38c5e592ebe6cdb3a80f6
kevinblum <blum.kevin@gmx.net>
2018-01-13 20:18:13 +0100

    Anpassungen

--- server.js
+++ server.js
@@ -16,7 +16,16 @@ app.set('view engine', 'pug');
 app.set('views', path.join('server', 'views'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.set('port', (process.env.PORT || 5000));
 
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
 /**************************************************************************************************
  * Define Express Middlewares
  **************************************************************************************************/
